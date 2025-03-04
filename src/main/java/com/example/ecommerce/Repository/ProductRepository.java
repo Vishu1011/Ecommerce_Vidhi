e6e863ea58ec4d6ea36aa04467d6b22ffc850412
Vishu@1011 <vidhiok@gmail.com>
2025-03-04 12:12:17 -0800

    done

--- src/main/java/com/example/ecommerce/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.Repository;
-
-import com.example.ecommerce.Entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository <Product, Long> {
-
-        // Product findByTrackingId(String trackingId);
-}
